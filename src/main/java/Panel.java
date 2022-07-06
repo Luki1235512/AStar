5e17b5a7337643e8b138eeb7c98653fc1a5323e2
Luki1235512 <Luki1235512@gmail.com>
2022-07-06 09:27:35 +0200

    Create and place node

--- src/main/java/Panel.java
+++ src/main/java/Panel.java
@@ -10,10 +10,28 @@ public class Panel extends JPanel {
     final int screenWidth = nodeSize * maxCol;
     final int screenHeight = nodeSize * maxRow;
 
+    // NODE
+    Node[][] node = new Node[maxCol][maxRow];
+
     public Panel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setLayout(new GridLayout(maxRow, maxCol));
+
+        // PLACE NODES
+        int col = 0;
+        int row = 0;
+
+        while (col < maxCol && row < maxRow) {
+            node[col][row] = new Node(col, row);
+            this.add(node[col][row]);
+
+            col++;
+            if (col == maxCol) {
+                col = 0;
+                row++;
+            }
+        }
     }
 }
